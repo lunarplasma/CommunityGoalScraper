837135f3a75a6bc36dd856ab94f2fe25ef2d3262
lunarplasma <lunarplasma@gmail.com>
2021-04-05 18:00:36 +0100

    Forcing UTF encoding

--- save.py
+++ save.py
@@ -10,7 +10,7 @@ def save_to_csv(filename: str, data: List[Dict]):
     if len(data) > 0:
         data_keys = data[0].keys()
 
-    with open(filename, 'a', newline='') as file:
+    with open(filename, 'a', newline='', encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=data_keys)
         if header_required:
             writer.writeheader()
